73c2371f27dabdde4f2a9b598fac5af6bde9929e
Arturo Silva <347490+artmsilva@users.noreply.github.com>
2025-01-30 22:10:06 -0500

    feat: update to latest astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,3 @@
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [image()]
-});
+export default defineConfig();
